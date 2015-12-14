277809d8b4fb24b6b256ff75a47117b6cd632f22
Laurent Dezitter <laurent.dezitter@gmail.com>
2015-12-14 15:02:23 +0100

    [build] fix seed task
    The 'created_at' and 'updated_at' timestamp fields were added as NOT NULL
    columns (see d834334 and 4c8fe92) but no values were added into the seed files.

--- app/feedr/services/database.rb
+++ app/feedr/services/database.rb
@@ -33,7 +33,21 @@ module Feedr
 
     private
       def seed_table(db, table, hashes)
+        timestamp_fields = get_timestamp_fields(db, table)
+        hashes = hashes.map { |h| stamp_hash(h, timestamp_fields) }
+
         db.from(table).multi_insert(hashes)
+      end
+
+      def get_timestamp_fields(db, table)
+        [:created_at, :updated_at] & db.from(table).columns
+      end
+
+      def stamp_hash(hash, timestamp_fields)
+        ts = DateTime.now
+        field_value_pairs = timestamp_fields.map { |f| [f, ts] }
+
+        hash.merge( Hash[field_value_pairs] )
       end
 
     end
