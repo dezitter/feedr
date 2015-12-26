b37e43f304dfee71e16e478df3b5ef60005c2bcf
Laurent Dezitter <laurent.dezitter@gmail.com>
2015-12-26 14:53:27 +0100

    [frontend] introduce api helper

---
+++ app/feedr/public/js/app/api/index.js
@@ -0,0 +1,23 @@
+import  { Promise } from 'es6-promise';
+import superagent from 'superagent';
+
+class Api {
+
+    static get(endpoint, options={}) {
+        return new Promise((resolve, reject) => {
+            superagent.get(endpoint)
+                      .query(options.query)
+                      .set('Accept', 'application/json')
+                      .end(onResponse);
+
+            function onResponse(err, response) {
+                if (err) { return reject(err); }
+
+                resolve(response.body);
+            }
+        });
+    }
+
+}
+
+export default Api;
--- app/feedr/public/js/app/fetcher/index.js
+++ app/feedr/public/js/app/fetcher/index.js
@@ -1,21 +1,9 @@
-import  { Promise } from 'es6-promise';
-import superagent from 'superagent';
+import api from 'app/api';
 
 class Fetcher {
 
     fetch(endpoint, options={}) {
-        return new Promise((resolve, reject) => {
-            superagent.get(endpoint)
-                      .query(options.query)
-                      .set('Accept', 'application/json')
-                      .end(onResponse);
-
-            function onResponse(err, response) {
-                if (err) { return reject(err); }
-
-                resolve(response.body);
-            }
-        });
+        return api.get(endpoint, options);
     }
 }
 
