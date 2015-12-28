5f5475e51c13592afa4865633edfaa3e3d73921d
Laurent Dezitter <laurent.dezitter@gmail.com>
2015-12-28 16:01:01 +0100

    [app] introduce helpers for js/css assets links

--- app/feedr/helpers/view.rb
+++ app/feedr/helpers/view.rb
@@ -11,5 +11,15 @@ module Feedr
       context.fn(this) if context[:hash].url == app.request.path
     end
 
+    def css(app, this, context, options)
+      href = context[:hash].href
+      "<link rel='stylesheet' href='#{href}'>"
+    end
+
+    def js(app, this, context, options)
+      src = context[:hash].src
+      "<script type='text/javascript' src='#{src}'></script>"
+    end
+
   end
 end
